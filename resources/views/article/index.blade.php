47e9a3070b848cfcc6f0e7aabce7abcd736b4e1a
Carmine <gonnellacarmine@gmail.com>
2023-03-03 16:20:56 +0100

    Gestione ruoli

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -15,16 +15,16 @@
         <div class="row justify-content-center">
             @foreach ($articles as $article)
                 <div class="col-12 col-md-8 col-lg-6">
-                    <div class="card">
-                        <img src="{{Storage::url($article->img)}}" alt="">
+                    <div class="card" style="width: 18rem">
+                        <img class="card-img-top" src="{{Storage::url($article->img)}}" alt="">
                         <div class="card-details">
                           <p class="text-title">{{$article->title}}</p>
                           <p class="text-title">{{$article->subtitle}}</p>
                           <p class="text-title">{{$article->category->name}}</p>  
-                          <a href="{{route('article.byCategory' , ['category'=>$article->category->id])}}">{{$article->category->name}}</a>                        
+                          <a class="text-dark color-hv" href="{{route('article.byCategory' , ['category'=>$article->category->id])}}">{{$article->category->name}}</a>                        
                         </div>
                         <div>
-                            Redatto il: {{$article->created_at->format('d/m/Y')}} da {{$article->user->name}}
+                            Redatto il: {{$article->created_at->format('d/m/Y')}} da <a class="text-dark color-hv" href="{{route('article.byUser' , ['user'=>$article->user->id])}}">{{$article->user->name}}</a>
                         </div>
                         <a type="submit" class="card-button btn" href="{{route('article.show', compact('article'))}}">Leggi</a>
                       </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,27 +4,27 @@
         {{session('message')}}
     </div>
     @endif
-    <div class="container-fluid p-5 text-center text-dark">
+    <div class="container-fluid text-center text-dark">
         <div class="row justify-content-center">
-            <h1 class="display-1 text-dark">
+            <h1 class="text-dark ">
                 Aulab Post
             </h1>
         </div>
     </div>
-    <div class="container-fluid p-5 text-center text-dark">
+    <div class="container-fluid p-3 text-center text-dark">
         <div class="row justify-content-center">
             @foreach ($articles as $article)
                 <div class="col-12 col-md-8 col-lg-6">
-                    <div class="card">
-                        <img src="{{Storage::url($article->img)}}" alt="">
+                    <div class="card" style="width: 18rem">
+                        <img class="card-img-top" src="{{Storage::url($article->img)}}" alt="">
                         <div class="card-details">
                           <p class="text-title">{{$article->title}}</p>
                           <p class="text-title">{{$article->subtitle}}</p>
                           <p class="text-title">{{$article->category->name}}</p>     
-                          <a href="{{route('article.byCategory' , ['category'=>$article->category->id])}}">{{$article->category->name}}</a>                     
+                          <a  class="color-hv text-dark" href="{{route('article.byCategory' , ['category'=>$article->category->id])}}">{{$article->category->name}}</a>                     
                         </div>
                         <div>
-                            Redatto il: {{$article->created_at->format('d/m/Y')}} da <a href="{{route('article.byUser' , ['user'=>$article->user->id])}}">{{$article->user->name}}</a> 
+                            Redatto il: {{$article->created_at->format('d/m/Y')}} da <a class="color-hv text-dark" href="{{route('article.byUser' , ['user'=>$article->user->id])}}">{{$article->user->name}}</a> 
                         </div>
                         <button type="submit" class="card-button" href="{{route('article.show', compact('article'))}}">Leggi</button>
                       </div>
