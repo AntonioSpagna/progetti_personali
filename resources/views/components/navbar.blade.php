e29a8ef53b92f05552b27d6d7334f2eef2611ca9
vincenzoliso <vince.liso@libero.it>
2023-03-07 18:52:59 +0100

    cancellate righe di codice inutili nella navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -86,91 +86,6 @@
  
 
 
-    {{-- <header>
-
-        <div class="container-fluid">
-
-            <div class="navb-logo">
-                <img src="{{Storage::url('/img/default.jpg')}}" alt="Logo">
-            </div>
-
-            <div class="navb-items d-none d-lg-flex d-xl-flex">
-
-                <div class="item">
-                    <a href="/">Home</a>
-                </div>
-
-                <div class="item">
-                    <a href="/services">Services</a>
-                </div>
-
-                <div class="item">
-                    <a href="/cases">Cases</a>
-                </div>
-
-                <div class="item">
-                    <a href="/about">About</a>
-                </div>
-
-                <div class="item-button">
-                    <a href="/contact" type="button">Let's talk</a>
-                </div>
-            </div>
-
-            <!-- Button trigger modal -->
-            <div class="mobile-toggler d-lg-none">
-                <a href="#" data-bs-toggle="modal" data-bs-target="#navbModal">
-                    <i class="fa-solid fa-bars"></i>
-                </a>
-            </div>
-
-            <!-- Modal -->
-            <div class="modal fade" id="navbModal" tabindex="-1" aria-labelledby="exampleModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-
-                        <div class="modal-header">
-                            <img src="/img/logo-variant.png" alt="Logo">
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
-                        </div>
-
-                        <div class="modal-body">
-                            
-                            <div class="modal-line">
-                                <i class="fa-solid fa-house"></i><a href="/">Home</a>
-                            </div>
-
-                            <div class="modal-line">
-                                <i class="fa-solid fa-bell-concierge"></i><a href="/services">Services</a>
-                            </div>
-
-                            <div class="modal-line">
-                                <i class="fa-solid fa-file-lines"></i> <a href="/cases">Cases</a>
-                            </div>
-
-                            <div class="modal-line">
-                                <i class="fa-solid fa-circle-info"></i><a href="/about">About</a>
-                            </div>
-
-                            <a href="/contact" class="navb-button" type="button">Let's talk</a>
-                        </div>
-
-                        <div class="mobile-modal-footer">
-                            
-                            <a target="_blank" href="#"><i class="fa-brands fa-instagram"></i></a>
-                            <a target="_blank" href="#"><i class="fa-brands fa-linkedin-in"></i></a>
-                            <a target="_blank" href="#"><i class="fa-brands fa-youtube"></i></a>
-                            <a target="_blank" href="#"><i class="fa-brands fa-facebook"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    </header> --}}
-
-    
 
 
     
@@ -221,11 +136,10 @@
     </div> 
   </header>
    --}}
-
-
+  
    <nav id="Main_Navbar" class="navbar  navbar-expand-lg p-0">
     <div class="container-fluid">
-      <a class="navbar-brand " href="#"><img class="logo" src="{{Storage::url('/img/logo.png')}}" alt=""></a>
+      <a class="navbar-brand " href="{{route('homepage')}}"><img class="logo" src="{{Storage::url('/img/logo.png')}}" alt=""></a>
       <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon "></span>
       </button>
